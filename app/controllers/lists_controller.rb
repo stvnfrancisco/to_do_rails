83a1dd58e09fbb2a8be242e77f6018bfdbf3e481
steven francisco <stvnfrancisco@yahoo.com>
2015-07-13 15:05:05 -0700

    some front end working... links to lists and back to index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,29 @@
 class ListsController < ApplicationController
   def index
-    @lists = List.all
-    render :index
-  end
+  @lists = List.all
+  render :index
 end
 
 def show
-   @list = List.find(params[:id])
-   render :show
- end
+  @list = List.find(params[:id])
+  render :show
+end
+
+def new
+  render :new
+end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to  lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
