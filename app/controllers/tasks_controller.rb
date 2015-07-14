e094b8226fdd597968e98b37b27b98db9db3e61b
steven francisco <stvnfrancisco@yahoo.com>
2015-07-14 08:45:11 -0700

    all files from weekend lessons added... Add task function still not working properly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,16 @@
 class TasksController < ApplicationController
-  def new
+  def create
     @list = List.find(params[:list_id])
-    @task = @list.tasks.new
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      redirect_to list_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713172255) do
+ActiveRecord::Schema.define(version: 20150714143902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
