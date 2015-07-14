f92cb2a5e7787fba4e1b2c3db3080bfd6b503508
steven francisco <stvnfrancisco@yahoo.com>
2015-07-14 07:54:24 -0700

    many additions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,23 @@
 class ListsController < ApplicationController
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+  
+  def new
+    render :new
+  end
+
+  def update
+    @list= List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
   def edit
     @list = List.find(params[:id])
   end
@@ -27,5 +46,5 @@ end
 
   private
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :description)
   end
